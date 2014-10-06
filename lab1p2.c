614c1fb1410d9d6b37e74779c8ca40a88e00a847
unknown <comeau@TUCPD047.vpn.mmsi.com>
2014-10-05 17:38:16 -0700

    Lab1 Part3 - Timer complete.

--- lab1p2.c
+++ lab1p2.c
@@ -8,15 +8,15 @@
 #include "lcd.h"
 
 // ******************************************************************************************* //
-// Configuration bits for CONFIG1 settings. 
+// Configuration bits for CONFIG1 settings.
 //
 // Make sure "Configuration Bits set in code." option is checked in MPLAB.
 //
-// These settings are appropriate for debugging the PIC microcontroller. If you need to 
+// These settings are appropriate for debugging the PIC microcontroller. If you need to
 // program the PIC for standalone operation, change the COE_ON option to COE_OFF.
 
-_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & 
-          BKBUG_ON & COE_ON & ICS_PGx1 & 
+_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF &
+          BKBUG_ON & COE_ON & ICS_PGx1 &
           FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768 )
 
 // ******************************************************************************************* //
@@ -34,66 +34,64 @@ _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & O
 #define PLLMODE         4               	  // On-chip PLL setting (Fosc)
 #define FCY             (XTFREQ*PLLMODE)/2    // Instruction Cycle Frequency (Fosc/2)
 
-#define BAUDRATE         115200       
-#define BRGVAL          ((FCY/BAUDRATE)/16)-1 
+#define BAUDRATE         115200
+#define BRGVAL          ((FCY/BAUDRATE)/16)-1
 
 // ******************************************************************************************* //
 
-// Varaible utilized to store the count that is incremented within the Timer 1 interrupt 
+// Variable utilized to store the count that is incremented within the Timer 1 interrupt
 // service routine every second.
 // Notes:
-//        1. This variable is declared are volatile becuase it is updated in the interrupt 
+//        1. This variable is declared are volatile becuase it is updated in the interrupt
 //           service routine but will be read in the main execution loop.
-//        2. Declared as unsigned char as the varaible will only store the values between 
+//        2. Declared as unsigned char as the varaible will only store the values between
 //           0 and 10.
-volatile unsigned char cnt;
-
+volatile int cnt;
+unsigned char command;
+volatile int state = 0;
+volatile int temp = 0;
 // ******************************************************************************************* //
 
+void DebounceDelay(){
+     TMR2 = 0;
+        IEC0bits.T2IE = 0;
+	IFS0bits.T2IF = 0;
+
+        // Set Timer 1's period value regsiter to value for 1us. (1:1 prescalar).
+        PR2 = 5000;
+
+        // Setup Timer 1 control register (T1CON) to:
+        //     TON           = 1     (start timer)
+        //     TCKPS1:TCKPS2 = 0    (set timer prescaler to 1:256)
+        //     TCS           = 0     (Fosc/2)
+        T2CON = 0x8000;
+
+        IFS0bits.T2IF = 0;
+        while(IFS0bits.T2IF == 0);
+        return;
+}
 int main(void)
 {
-	// RPINR18 is a regsiter for selectable input mapping (see Table 10-2) for 
-	// for UART1. U1RX is 8 bit value used to specifiy connection to which
-	// RP pin. RP9 is used for this configuration. Physical Pin 18.
-	RPINR18bits.U1RXR = 9;	
-
-	// RPOR4 is a register for selctable ouput mapping (see Regsiter 1019) for
-	// pins RP9 and RP8. The register for RP8 is assigned to 3 to connect 
-	// the U1TX output for UART1 (see table 10-3). Physical Pin 17.
-	RPOR4bits.RP8R = 3;		
-
-	// Set UART1's baud rate generator register (U1BRG) to the value calculated above.
-	U1BRG  = BRGVAL;
-
-	// Set UART1's mode register to 8-bit data, no parity, 1 stop bit, enabled.
-	//     UARTEN        = 1     (enable UART)
-	//     PDSEL1:PDSEL0 = 00    (8-bit data, no parity)
-	//     STSEL         = 0     (1 stop bit)
-	U1MODE = 0x8000; 		
-
-	// Set UART2's status and control register
-	//     UTXISEL1:UTXISEL0 = 00    (U1TXIF set when character 
-	//                                written to trasmit buffer)
-	//     UTXEN             = 1     (trasnmit enabled)
-	//     URXISEL1:URXISEL0 = 01    (U1RXIF set when any character 
-	//                                is received in receive buffer)
-	//     RIDLE             = 0     (Reciver is active)
-	U1STA  = 0x0440; 		// Reset status register and enable TX & RX
-
-	// Clear the UART RX interrupt flag. Althouhg we are not using a ISR for 
-	// the UART receive, the UART RX interrupt flag can be used to deermine if 
-	// we have recived a character from the UART. 
-	IFS0bits.U1RXIF = 0;
 
 	// The following provides a demo configuration of Timer 1 in which
-	// the Timer 1 interrupt service routine will be executed every 1 second 
-	PR1 = 57599;			
-	TMR1 = 0;				
-	IFS0bits.T1IF = 0;		
+	// the Timer 1 interrupt service routine will be executed every 1 second
+	
+    //PR1 = 57599/100;
+
+	TMR1 = 0;
+        PR1 = 57599/100;
+        AD1PCFGbits.PCFG4 = 1;
+	IFS0bits.T1IF = 0;
 	IEC0bits.T1IE = 1;
 	T1CONbits.TCKPS = 3;
 	T1CONbits.TON = 1;
-
+        TRISBbits.TRISB5 = 1;
+        TRISBbits.TRISB2 = 1;
+        CNPU1bits.CN6PUE = 1;
+        CNEN2bits.CN27IE = 1;
+        CNEN1bits.CN6IE = 1;
+        IFS1bits.CNIF = 0;
+        IEC1bits.CNIE = 1;
 	// printf by default is mapped to serial communication using UART1.
 	// NOTES:
 	//        1. You must specify a heap size for printf. This is required
@@ -102,51 +100,199 @@ int main(void)
 	//           a.) Selecting Build Options...->Project from the Project menu.
 	//           b.) Selecting the MPLABLINK30 Tab.
 	//           c.) Entering the size of heap, e.g. 512, under Heap Size
-	//        2. printf function is advanced and using printf may require 
-	//           significant code size (6KB-10KB).   
+	//        2. printf function is advanced and using printf may require
+	//           significant code size (6KB-10KB).
 	printf("Lab 2: Debugging Statements\n\r");
 
-	// The following code will not work until you have implemented the 
+	// The following code will not work until you have implemented the
 	// the required LCD functions defined within lcd.c
 	LCDInitialize();
+/*******************************/
+////	//below is for testing MoceCursor command
+////	LCDMoveCursor(0,2);
+////	LCDPrintString("Hello");
+////	LCDMoveCursor(1,2);
+////	LCDPrintString("Test");
+////	command = 0xC;
+/*******************************/
+
 	LCDPrintString("Running:");
 	LCDMoveCursor(1,0);
-	LCDPrintChar('0');
-	LCDPrintChar('0');
-	LCDPrintChar(':');
-	LCDPrintChar('0');
-	LCDPrintChar('0');
-	LCDPrintChar('.');
-	LCDPrintChar('0');
-	LCDPrintChar('0');
+	LCDPrintString("00:00.00");
+//	LCDPrintChar('0');
+//	LCDPrintChar('0');
+//	LCDPrintChar(':');
+//	LCDPrintChar('0');
+//	LCDPrintChar('0');
+//	LCDPrintChar('.');
+//	LCDPrintChar('0');
+//	LCDPrintChar('0');
 
 	while(1)
 	{
-		LCDMoveCursor(1,4);
-		LCDPrintChar(cnt+'0');		
-	}
+            
+//             if(PORTBbits.RB5 == 0)
+//             {
+//                state = 2;
+//             }
+//             else
+//             {
+//                 state = 0;
+//             }
+
+            switch (state){
+                case 0:                 //initial
+                    TMR1 = 0;
+                    LCDClear();
+                    LCDMoveCursor(0,0);
+                    LCDPrintString("Stop");
+                    LCDMoveCursor(1,0);
+                    LCDPrintString("00:00.00");
+                    break;
+                case 1:                 //debounce
+                   DebounceDelay();
+                   
+                    TMR1 = 0;
+                    cnt = 0;
+                    break;
+                case 2:
+                    LCDMoveCursor(0,0);
+                    LCDPrintString("Run!");
+                    LCDMoveCursor(1,0);
+                    LCDPrintChar((cnt/60000)%10+'0');
+                    LCDMoveCursor(1,1);
+                    LCDPrintChar((cnt/6000)%10+'0');
+                    LCDMoveCursor(1,3);
+                    LCDPrintChar((cnt/1000)%10+'0');
+		    LCDMoveCursor(1,4);
+		    LCDPrintChar((cnt/100)%10+'0');
+                    LCDMoveCursor(1,6);
+                    LCDPrintChar((cnt/10)%10+'0');
+                    LCDMoveCursor(1,7);
+                    LCDPrintChar(cnt%10+'0');
+                    break;
+                case 3:
+                    LCDMoveCursor(0,0);
+                    LCDPrintString("Wait");
+                    LCDMoveCursor(1,0);
+                    TMR1 = temp;
+                    break;
+                case 4:                 //debounce
+                    DebounceDelay();
+
+                    TMR1 = 0;
+                   
+                    break;
+                case 5:                 //debounce
+                     DebounceDelay();
+                    TMR1 = temp;
+                    break;
+                case 6:                 //debounce
+                     DebounceDelay();
+                    TMR1 = 0;
+                    cnt = 0 ;
+                    break;
+            }
+            
+    	}
 	return 0;
 }
 
 // ******************************************************************************************* //
 // Defines an interrupt service routine that will execute whenever Timer 1's
 // count reaches the specfied period value defined within the PR1 register.
-// 
-//     _ISR and _ISRFAST are macros for specifying interrupts that 
-//     automatically inserts the proper interrupt into the interrupt vector 
+//
+//     _ISR and _ISRFAST are macros for specifying interrupts that
+//     automatically inserts the proper interrupt into the interrupt vector
 //     table
 //
 //     _T1Interrupt is a macro for specifying the interrupt for Timer 1
 //
 // The functionality defined in an interrupt should be a minimal as possible
-// to ensure additional interrupts can be processed. 
-void _ISR _T1Interrupt(void)
+// to ensure additional interrupts can be processed.
+
+void __attribute__((interrupt,auto_psv)) _T1Interrupt(void)
+//void _ISR _T1Interrupt(void)
 {
 	// Clear Timer 1 interrupt flag to allow another Timer 1 interrupt to occur.
-	IFS0bits.T1IF = 0;		
-	
+	IFS0bits.T1IF = 0;
+
 	// Updates cnt to wraparound from 9 to 0 for this demo.
-	cnt = (cnt<9)?(cnt+1):0;
+	cnt = cnt + 1;
+
+/*******************************/
+	//make the LCD blink;
+////	command ^= 0x4;
+////	WriteLCD(command, 0, 40);
+/*******************************/
+}
+
+void __attribute__((interrupt,auto_psv)) _CNInterrupt(void)
+{
+	// Clear CN interrupt flag to allow another CN interrupt to occur.
+	IFS1bits.CNIF = 0;
+        if(state == 0 && PORTBbits.RB2 == 1)
+        {
+            state = 0;
+        }
+        else if(state == 0 && PORTBbits.RB2 == 0)
+        {
+            state = 1;
+        }
+        else if(state == 1 && PORTBbits.RB2 == 0)
+        {
+            state = 1;
+        }
+         else if(state == 1 && PORTBbits.RB2 == 1)
+        {
+            state = 2;
+            cnt = 0;
+        }
+          else if(state == 2 && PORTBbits.RB2 == 1)
+        {
+            state = 2;
+        }
+          else if(state == 2 && PORTBbits.RB2 == 0)
+        {
+            state = 4;
+        }
+
+           else if(state == 4 && PORTBbits.RB2 == 0)
+        {
+            state = 4;
+            temp = TMR1;
+        }
+            else if(state == 4 && PORTBbits.RB2 == 1)
+        {
+            state = 3;
+            temp = TMR1;
+        }
+            else if(state == 3 && PORTBbits.RB2 == 1)
+        {
+            state = 3;
+            temp = TMR1;
+        }
+
+           else if(state == 3 && PORTBbits.RB2 == 0)
+        {
+            state = 5;
+        }
+            else if(state == 5 && PORTBbits.RB2 == 0)
+        {
+            state = 5;
+        }
+            else if(state == 5 && PORTBbits.RB2 == 1)
+        {
+            state = 2;
+        }
+           if(state != 2 && PORTBbits.RB5 == 0)
+        {
+            state = 0;
+        }
+//    if(PORTBbits.RB5 == 0)
+//    {
+//        state = 2;
+//    }
 }
 
 // ******************************************************************************************* //
